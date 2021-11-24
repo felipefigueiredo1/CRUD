f2d8aff990bf3c60b6e4f6a9b8bf78a49bc317ef
Felipe Figueiredo <92311384+felipefigueiredo1@users.noreply.github.com>
2021-11-24 15:38:32 -0300

    Update editar.php

--- php/editar.php
+++ php/editar.php
@@ -5,8 +5,8 @@ include 'conexao.php';
 $id = $_POST['id'];
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$telefone = $_POST['telefone'];
-$recebendo_cadastros = ("UPDATE cliente SET nome = '$nome', email = '$email', telefone = '$telefone'
+$senha = $_POST['senha'];
+$recebendo_cadastros = ("UPDATE cliente SET nome = '$nome', email = '$email', senha = '$senha'
 WHERE id = '$id'");
 $query_cadastros = mysqli_query($conexao,$recebendo_cadastros) or die(mysqli_error($conexao));
 
